19968f8087a92e925c88d37e0b9be93bddbcb180
gef3dx <gef3dx@gmail.com>
2023-12-14 18:52:10 +0300

    step 2

---
+++ core/models/product.py
@@ -0,0 +1,8 @@
+from sqlalchemy.orm import Mapped
+from core.models.base import Base
+
+
+class Product(Base):
+    name: Mapped[str]
+    description: Mapped[str]
+    price: Mapped[int]
--- main.py
+++ main.py
@@ -1,10 +1,26 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 import uvicorn
+
+from core.config import settings
+from core.models import Base, db_helper
+from api_v1 import router as router_v1
 from items_views import item_router
 from users.views import router_user
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 app.include_router(item_router)
 app.include_router(router_user)
 
@@ -12,17 +28,24 @@ app.include_router(router_user)
 @app.get("/")
 def hello_index():
     return {
-        "message": "Hello index",
+        "message": "Hello index!",
     }
 
 
 @app.get("/hello/")
-def hello(name: str = "Hello"):
+def hello(name: str = "World"):
     name = name.strip().title()
+    return {"message": f"Hello {name}!"}
+
+
+@app.get("/calc/add/")
+def add(a: int, b: int):
     return {
-        "message": f"Hello {name}!"
+        "a": a,
+        "b": b,
+        "result": a + b,
     }
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
